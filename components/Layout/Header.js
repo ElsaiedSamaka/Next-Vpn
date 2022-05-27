71b19d3493efbf3132802417395deda41e394902
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-05-27 23:19:17 +0200

    tiny updates

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import React, { useEffect, useState } from "react";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import ButtonOutline from "../misc/ButtonOutline.";
-import LogoVPN from "../../public/assets/Logo.svg";
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -21,14 +20,14 @@ const Header = () => {
           (scrollActive ? " shadow-md pt-0" : " pt-4")
         }
       >
-        <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
-          <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-8 w-auto" />
+        <nav className='max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4'>
+          <div className='col-start-1 col-end-2 flex items-center'>
+            <strong>NextVPN</strong>
           </div>
-          <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
+          <ul className='hidden lg:flex col-start-4 col-end-8 text-black-500  items-center'>
             <LinkScroll
-              activeClass="active"
-              to="about"
+              activeClass='active'
+              to='about'
               spy={true}
               smooth={true}
               duration={1000}
@@ -45,8 +44,8 @@ const Header = () => {
               About
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="feature"
+              activeClass='active'
+              to='feature'
               spy={true}
               smooth={true}
               duration={1000}
@@ -63,8 +62,8 @@ const Header = () => {
               Feature
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="pricing"
+              activeClass='active'
+              to='pricing'
               spy={true}
               smooth={true}
               duration={1000}
@@ -81,8 +80,8 @@ const Header = () => {
               Pricing
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="testimoni"
+              activeClass='active'
+              to='testimoni'
               spy={true}
               smooth={true}
               duration={1000}
@@ -99,9 +98,9 @@ const Header = () => {
               Testimonial
             </LinkScroll>
           </ul>
-          <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
-            <Link href="/">
-              <a className="text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-orange-500 transition-all">
+          <div className='col-start-10 col-end-12 font-medium flex justify-end items-center'>
+            <Link href='/'>
+              <a className='text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-orange-500 transition-all'>
                   Sign In
               </a>
             </Link>
@@ -111,12 +110,12 @@ const Header = () => {
       </header>
       {/* Mobile Navigation */}
 
-      <nav className="fixed lg:hidden bottom-0 left-0 right-0 z-20 px-4 sm:px-8 shadow-t ">
-        <div className="bg-white-500 sm:px-3">
-          <ul className="flex w-full justify-between items-center text-black-500">
+      <nav className='fixed lg:hidden bottom-0 left-0 right-0 z-20 px-4 sm:px-8 shadow-t '>
+        <div className='bg-white-500 sm:px-3'>
+          <ul className='flex w-full justify-between items-center text-black-500'>
             <LinkScroll
-              activeClass="active"
-              to="about"
+              activeClass='active'
+              to='about'
               spy={true}
               smooth={true}
               duration={1000}
@@ -131,24 +130,24 @@ const Header = () => {
               }
             >
               <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+                className='w-6 h-6'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   strokeWidth={2}
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
                 />
               </svg>
               About
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="feature"
+              activeClass='active'
+              to='feature'
               spy={true}
               smooth={true}
               duration={1000}
@@ -163,24 +162,24 @@ const Header = () => {
               }
             >
               <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+                className='w-6 h-6'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   strokeWidth={2}
-                  d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                  d='M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9'
                 />
               </svg>
               Feature
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="pricing"
+              activeClass='active'
+              to='pricing'
               spy={true}
               smooth={true}
               duration={1000}
@@ -195,24 +194,24 @@ const Header = () => {
               }
             >
               <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+                className='w-6 h-6'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   strokeWidth={2}
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  d='M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
                 />
               </svg>
               Pricing
             </LinkScroll>
             <LinkScroll
-              activeClass="active"
-              to="testimoni"
+              activeClass='active'
+              to='testimoni'
               spy={true}
               smooth={true}
               duration={1000}
@@ -227,17 +226,17 @@ const Header = () => {
               }
             >
               <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+                className='w-6 h-6'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                   strokeWidth={2}
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                  d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
                 />
               </svg>
               Testimonial
