85007d623264216aa5a0f5a5b4dfb87c25eb1d92
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-05-27 21:40:31 +0200

    initial commit - create next app

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res, next) => {
-  res.send({ message: "Successful" });
-});
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("server started on : ", PORT);
-});
